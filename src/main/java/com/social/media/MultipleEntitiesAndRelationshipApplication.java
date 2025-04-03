89e084b4153d83bab71fdb7549e0a02c9fcad65b
Ramanuj <anuj.mca.nitrr.2019@gmail.com>
2025-04-03 11:28:56 +0530

    Initial commit

---
+++ src/main/java/com/social/media/MultipleEntitiesAndRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.social.media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleEntitiesAndRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleEntitiesAndRelationshipApplication.class, args);
+	}
+
+}
